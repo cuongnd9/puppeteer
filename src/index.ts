61d6f89bf50b9501f42ad4972bcee44b1305870e
Cuong Duy Nguyen <103cuong@gmail.com>
2020-08-08 21:39:14 +0700

    build: done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { logger } from 'juno-js';
+import puppeteer from 'puppeteer'
+import { logger } from 'juno-js'
 
-logger().info('Chao Xìn 🇻🇳');
+(async () => {
+  try {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.setViewport({
+      width: 1600,
+      height: 1200,
+    })
+    await page.goto('https://github.com/103cuong', { waitUntil: 'networkidle2' })
+
+    await page.screenshot({ path: 'assets/103cuong.png' })
+    await page.pdf({ path: 'assets/103cuong.pdf', format: 'A4' })
+    await browser.close()
+  } catch(error) {
+    logger().error(error)
+  }
+})()
